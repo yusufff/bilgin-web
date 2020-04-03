8d5986d682d105a517ec3a2f01ec4b459bd6072e
Yusuf YILDIZ <a.yusuf.yildiz@hotmail.com>
2020-04-04 02:11:56 +0300

    Updates

---
+++ precache-manifest.359efb95763c06d8ad98fdf79cbfdee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631fa882e6cfa56340b52d566c69151a",
+    "url": "/bilgin-web/index.html"
+  },
+  {
+    "revision": "1383e9e10acaea6f7537",
+    "url": "/bilgin-web/static/js/2.63219876.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bilgin-web/static/js/2.63219876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff22766e5cf89f5ffd75",
+    "url": "/bilgin-web/static/js/main.d4821563.chunk.js"
+  },
+  {
+    "revision": "47a97b058d5fb80d8bb7",
+    "url": "/bilgin-web/static/js/runtime-main.1459bdde.js"
+  },
+  {
+    "revision": "ad66e362e1b6b192d3fb88a7af12b8e9",
+    "url": "/bilgin-web/static/media/logo.ad66e362.svg"
+  }
+]);
