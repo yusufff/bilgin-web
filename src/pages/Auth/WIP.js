6d5b0ea4d6c375dd4e1d15dce9393a860aa13394
Yusuf YILDIZ <a.yusuf.yildiz@hotmail.com>
2020-04-06 17:36:20 +0300

    Open Login and Register

--- src/pages/Auth/WIP.js
+++ src/pages/Auth/WIP.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
+import { Heading, Button } from 'grommet';
 
 import Intro from '../../components/Intro';
 
 import AuthWrapper from './AuthWrapper';
-import { Heading } from 'grommet';
 
 function Login() {
+  const history = useHistory();
+
+  const goToLogin = () => {
+    history.push('/giris');
+  }
+  const goToRegister = () => {
+    history.push('/kayit');
+  }
+
   return (
     <AuthWrapper>
       <Intro />
-      <Heading fill level="2" textAlign="center">Teknik aksaklıklardan dolayı kayıtlar 6 Nisan 18.30'da açılacaktır. Yarışma 21:00'da başlayacaktır</Heading>
+      <Heading fill level="3" textAlign="center">Yarışma 21:00'da başlayacaktır</Heading>
+      <Button onClick={goToLogin} primary label="GİRİŞ YAP" margin={{ bottom: '10px' }} />
+      <Button onClick={goToRegister} primary label="KAYIT OL" />
     </AuthWrapper>
   )
 }
